8b0f65b7f2877b368a619a6bdbae2bff089c4985
dc <dincaus@gmail.com>
2016-04-18 10:50:32 +0200

    Work on tests

---
+++ tests/test1.py
@@ -0,0 +1,26 @@
+import unittest
+from mock import patch, Mock
+
+
+class TestCase1(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual("test", "test")
+
+    def test2(self):
+        self.assertEqual("test", "test")
+
+    def test3(self):
+        self.assertEqual("test", "test")
+
+    def test4(self):
+        self.assertEqual("test", "test")
+
+    def test5(self):
+        self.assertEqual("test", "test")
+
+    def test6(self):
+        self.assertEqual("test", "test")
+
+if __name__ == '__main__':
+    unittest.main()
